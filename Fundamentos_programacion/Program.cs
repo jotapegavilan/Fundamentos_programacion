2d1088d483055e11af7238d3971654560cb4f970
Jotape <j.p.daniel.gavilan@gmail.com>
2021-04-19 18:10:11 -0400

    Agregar archivos de proyecto.

---
+++ Fundamentos_programacion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos_programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sintaxis
+            sumarAB();
+            Console.ReadKey();
+
+        }
+
+        static void sumarAB()
+        {
+            int x = 0;
+            int a = 14;
+            int b = 60;
+            x = a + b;
+            Console.WriteLine(x);
+        }
+
+
+    }
+}
